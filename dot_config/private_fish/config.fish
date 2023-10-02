7a427b0a925123fab09e9a93af2fb0dfb7f5287c
Connor Edwards <38229097+cedws@users.noreply.github.com>
2023-10-02 02:52:28 +0100

    Add k9s config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,16 +6,19 @@ if status is-interactive
     set -gx HOMEBREW_PREFIX "/opt/homebrew";
     set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
     set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-    set -gx HOMEBREW_SHELLENV_PREFIX "/opt/homebrew";
+
+    set -q MANPATH; or set MANPATH ''
+    set -gx MANPATH "/opt/homebrew/share/man" $MANPATH
+    set -q INFOPATH; or set INFOPATH ''
+    set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH
 
     fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
-    fish_add_path /opt/homebrew/share/man
-    fish_add_path /opt/homebrew/share/info
     fish_add_path /opt/homebrew/opt/openjdk@11/bin;
     fish_add_path ~/go/bin
 
     set -x VISUAL nano
     set -x EDITOR $VISUAL
+    set -x K9SCONFIG ~/.config/k9s
 
     alias rg="rg --no-ignore -F"
     alias fd="fd --no-ignore"
