a3b06c397b10356caca5e2d566045cb1930e312c
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-08-02 06:02:12 +0900

    Fix fish config

--- dot_config/private_fish/conf.d/30_path.fish
+++ dot_config/private_fish/conf.d/30_path.fish
@@ -1,5 +1,7 @@
-fish_add_path (brew --prefix)/opt/openjdk/bin;
-fish_add_path (brew --prefix)/opt/libpq/bin
+if command -q brew
+    fish_add_path (brew --prefix)/opt/openjdk/bin;
+    fish_add_path (brew --prefix)/opt/libpq/bin
+end
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.orbstack/bin
 fish_add_path ~/go/bin
