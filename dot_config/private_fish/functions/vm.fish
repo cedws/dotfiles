0741df7eb97892eeb305660ade0dd898e7e98a30
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-08-03 21:32:50 +0900

    Add vm fish function

--- dot_config/private_fish/functions/vm.fish
+++ dot_config/private_fish/functions/vm.fish
@@ -1,9 +1,9 @@
 function vm --description "Start a macOS VM with a mounted path"
-    argparse 'vm-name=' 'mount-path=' -- $argv
+    argparse 'vm-name=' 'mount-path=' 'no-graphics' -- $argv
     or return 1
 
     if not set -q _flag_vm_name; or not set -q _flag_mount_path
-        echo "Usage: vm --vm-name <name> --mount-path <path>"
+        echo "Usage: vm --vm-name <name> --mount-path <path> [--no-graphics]"
         return 1
     end
 
@@ -12,7 +12,12 @@ function vm --description "Start a macOS VM with a mounted path"
 
     tart clone ghcr.io/cirruslabs/macos-sequoia-base:latest $VM_NAME
 
-    tart run --dir $MOUNT_PATH $VM_NAME &
+    set tart_args --no-audio --no-clipboard --dir $MOUNT_PATH
+    if set -q _flag_no_graphics
+        set -a tart_args --no-graphics
+    end
+
+    tart run $tart_args $VM_NAME &
     set tart_pid $last_pid
 
     sshpass -p admin ssh -o "StrictHostKeyChecking no" -o "SetEnv TERM=xterm-256color" admin@(tart ip --wait 30 $VM_NAME)
