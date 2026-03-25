647389b595196e83a8493b7c32e9aadd9a10f8fe
Connor Edwards <38229097+cedws@users.noreply.github.com>
2026-03-25 22:08:06 +0000

    Update terminal settings

--- nix/modules/terminal.nix
+++ nix/modules/terminal.nix
@@ -1,44 +1,33 @@
 { config, pkgs, lib, ... }:
 
 {
-  programs.wezterm = {
-    enable = true;
-
-    extraConfig = ''
-      local wezterm = require 'wezterm'
-
-      local config = wezterm.config_builder()
-
-      config.color_scheme = 'Ayu Mirage'
-      config.font = wezterm.font 'JetBrains Mono'
-      config.default_cursor_style = 'SteadyBar'
-
-      return config
-    '';
-  };
-
   xdg.configFile."ghostty/config".text = ''
     title = "Ghostty"
     theme = "Ayu Mirage"
 
+    background-opacity = 0.8
     clipboard-read = allow
     copy-on-select = false
     cursor-style = bar
     focus-follows-mouse = true
     macos-non-native-fullscreen = true
-    macos-option-as-alt = left
+    macos-option-as-alt = true
     macos-titlebar-style = tabs
     macos-window-shadow = false
     mouse-hide-while-typing = true
     quit-after-last-window-closed = true
     shell-integration-features = no-cursor,no-sudo,no-title
     unfocused-split-opacity = 0.8
+    window-padding-x = 8
+    window-padding-y = 8
     window-save-state = always
     window-theme = system
 
     font-family = "JetBrains Mono"
     font-size = 16
 
+    keybind = alt+2=text:€
+    keybind = alt+3=text:#
     keybind = cmd+right=text:\x05
     keybind = alt+left=esc:b
     keybind = alt+right=esc:f
