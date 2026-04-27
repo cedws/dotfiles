083aae00a59ccfffb0678ecc638cf7ca099671a9
Connor Edwards <38229097+cedws@users.noreply.github.com>
2026-04-27 21:25:26 +0100

    Set up ghostty global term and keybinds

--- nix/modules/terminal.nix
+++ nix/modules/terminal.nix
@@ -14,7 +14,6 @@
     macos-window-shadow = false
     mouse-hide-while-typing = true
     quit-after-last-window-closed = true
-    shell-integration-features = no-cursor,no-sudo,no-title
     unfocused-split-opacity = 0.8
     window-padding-x = 8
     window-padding-y = 8
@@ -23,6 +22,21 @@
 
     font-family = "JetBrains Mono"
     font-size = 16
+
+    keybind = global:cmd+backquote=toggle_quick_terminal
+    quick-terminal-position = top
+    quick-terminal-screen = mouse
+    quick-terminal-animation-duration = 0
+    quick-terminal-autohide = true
+
+    keybind = cmd+d=new_split:right
+    keybind = cmd+shift+d=new_split:down
+    keybind = cmd+ctrl+left=goto_split:left
+    keybind = cmd+ctrl+right=goto_split:right
+    keybind = cmd+ctrl+up=goto_split:up
+    keybind = cmd+ctrl+down=goto_split:down
+    keybind = cmd+shift+enter=toggle_split_zoom
+    keybind = cmd+shift+e=equalize_splits
 
     keybind = alt+2=text:€
     keybind = alt+3=text:#
